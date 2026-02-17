b42cb4318de906468ca1b8eba71978d5312ba983
Armands <ast2@inbox.lv>
2026-02-17 23:45:54 +0200

    drag and drop

--- Assets/Scripts/DragScript.cs
+++ Assets/Scripts/DragScript.cs
@@ -1,44 +1,44 @@
-// using UnityEngine;
-// using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
-// public class DragScript : MonoBehaviour, 
-//     IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
+public class DragScript : MonoBehaviour, 
+    IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
-//     private RectTransform trans;
-//     public Canvas canvas;
-//     // SFXScript sfxScript;
+    private RectTransform trans;
+    public Canvas canvas;
+    SFXScript sfxScript;
     
-//     // Start is called once before the first execution of Update after the MonoBehaviour is created
-//     void Start()
-//     {
-//         sfxScript = FindFirstObjectByType<SFXScript>();
-//         trans = GetComponent<RectTransform>();
-//     }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // sfxScript = FindFirstObjectByType<SFXScript>();
+        trans = GetComponent<RectTransform>();
+    }
 
-//    public void OnPointerDown(PointerEventData data) {
-//         Debug.Log("Užklikšķināts uz velkama objekta!");
-//         sfxScript.PlaySFX(0);
-//     }
+   public void OnPointerDown(PointerEventData data) {
+        Debug.Log("Užklikšķināts uz velkama objekta!");
+        // sfxScript.PlaySFX(0);
+    }
 
-//     public void OnBeginDrag(PointerEventData data) {
-//         Debug.Log("Sākta objekta vilkšana!");
-//     }
+    public void OnBeginDrag(PointerEventData data) {
+        Debug.Log("Sākta objekta vilkšana!");
+    }
 
-//     public void OnDrag(PointerEventData data) {
-//         Debug.Log("Notiek vilkšana!");
-//         Vector2 mousePosition = data.position;
-//         mousePosition.x = Mathf.Clamp(mousePosition.x, 
-//             0 + trans.rect.width / 2, 
-//             Screen.width - trans.rect.width / 2);
+    public void OnDrag(PointerEventData data) {
+        Debug.Log("Notiek vilkšana!");
+        Vector2 mousePosition = data.position;
+        mousePosition.x = Mathf.Clamp(mousePosition.x, 
+            0 + trans.rect.width / 2, 
+            Screen.width - trans.rect.width / 2);
 
-//         mousePosition.y = Mathf.Clamp(mousePosition.y,
-//            0 + trans.rect.height / 2,
-//            Screen.height - trans.rect.height / 2);
+        mousePosition.y = Mathf.Clamp(mousePosition.y,
+           0 + trans.rect.height / 2,
+           Screen.height - trans.rect.height / 2);
 
-//         trans.position = mousePosition;
-//     }
+        trans.position = mousePosition;
+    }
 
-//     public void OnEndDrag(PointerEventData data) {
-//         Debug.Log("Beigusies objekta vilkšana!");
-//     }
-// }
+    public void OnEndDrag(PointerEventData data) {
+        Debug.Log("Beigusies objekta vilkšana!");
+    }
+}
